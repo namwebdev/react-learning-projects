bcd99011d8ac04a2e2e2092ea7b8c68a22d9c8f8
@jvit-phuongnam <nam.job96@gmail.com>
2024-09-28 18:29:16 +0700

    - Add Sidebar with collapse

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import { Sidebar } from "@/components/Sidebar/Sidebar";
-
+import { SidebarWithCollapse } from "@/components/Sidebar/SidebarWithCollapse";
 
 export default async function Layout({
   children,
@@ -8,7 +8,8 @@ export default async function Layout({
 }>) {
   return (
     <section className="grid sm:grid-cols-[220px_1fr] grid-cols-[1fr] gap-4">
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <SidebarWithCollapse />
       <main>{children}</main>
     </section>
   );
