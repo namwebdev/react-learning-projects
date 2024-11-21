c2efe373b2f514e452450a2ce20367ff689ea99a
@jvit-phuongnam <nam.job96@gmail.com>
2024-11-21 20:05:02 +0700

    - Add util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { differenceInYears, format, formatDistance } from 'date-fns';
-
+import { differenceInYears, format, formatDistance } from "date-fns";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function calculateAge(dob: Date) {
-    return differenceInYears(new Date(), dob);
+  return differenceInYears(new Date(), dob);
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
